f85b7c0aa29220ef9ef34725e588328430bf7650
yiming543 <yiming.shi@true-win.com>
2026-03-06 08:35:57 +0800

    V05
    //20260305 V05 CS:D5F8
    //修正迎賓燈一直送會重覆啟動迎賓燈

---
+++ TY1500_U3_V05.X/LED.h
@@ -0,0 +1,150 @@
+/*
+ * File:   LED.h
+ * Author: T00904
+ *
+ * Created on January 8, 2026, 11:30 AM
+ */
+
+#ifndef LED_H
+#define LED_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mcc_generated_files/mcc.h"
+#include <xc.h>
+
+typedef union {
+  struct {
+    unsigned TURN : 1;
+    unsigned DRL : 1;
+    unsigned POS : 1;
+    unsigned WHITE : 1;
+    unsigned TEST : 1;
+    unsigned WELCOME : 1;
+    unsigned b6 : 1;
+    unsigned b7 : 1;
+  };
+  uint8_t Byte;
+} sflag;
+
+const uint8_t BIT_TURN= 0x01;
+const uint8_t BIT_DRL= 0x01<<1;
+const uint8_t BIT_POS= 0x01<<2;
+const uint8_t BIT_WHITE= 0x01<<3;
+const uint8_t BIT_TEST= 0x01<<4;
+const uint8_t BIT_WELCOME= 0x01<<5;
+
+typedef enum {
+  TURN_STEP_0 = 0,
+  TURN_STEP_1,
+  TURN_STEP_2,
+  TURN_STEP_3,
+  TURN_STEP_4,
+} eTURN_STEP;
+
+typedef enum {
+  WELCOME_STEP_0 = 0,
+  WELCOME_STEP_1,
+  WELCOME_STEP_2,
+  WELCOME_STEP_3,
+  WELCOME_STEP_4,
+  WELCOME_STEP_5,
+  WELCOME_STEP_6,
+  WELCOME_STEP_7,
+  WELCOME_STEP_8,
+  WELCOME_STEP_9,
+  WELCOME_STEP_10,
+  WELCOME_STEP_11,
+  WELCOME_STEP_12,
+  WELCOME_STEP_13,
+  WELCOME_STEP_14,
+} eWELCOME_STEP;
+
+//extern
+extern sflag ORB;
+extern const uint16_t PWM[];
+
+//ISR
+void tmr0_10ms_even(void);
+void clear_no_call_warning_message(void);
+
+void PWM_TURN_ON(void);
+void DRL_on(void);
+void POS_on(void);
+void TURN_LIGHT_ON(void);
+void all_led_off(void);
+
+void all_yellow_led_turn_on(void);
+void all_yellow_led_turn_off(void);
+void all_white_led_turn_on(void);
+void all_white_led_turn_off(void);
+
+bool is_white(void);
+bool is_turn_signals(void);
+bool is_test(void);
+bool is_DRL(void);
+bool is_POS(void);
+bool is_WELCOME(void);
+void LED_main(void);
+
+void PWM_DRL(void);
+void PWM_POS(void);
+void PWM_turn_signals(void);
+void PWM_all_off(void);
+
+void debounce_IO(void);
+void check_turn_signals(void);
+void turn_on_turn_signals(void);
+void delay_us(int8_t us);
+void delay_ms(uint16_t ms);
+void check_test_mode(void);
+void check_POS_P2(void);
+
+void white_fad_out(void);
+void yellow_to_white(void);
+void yellow_seg1_forward(void);
+void yellow_seg2_forward(void);
+void yellow_seg3_forward(void);
+void yellow_seg4_forward(void);
+void tellow_seg1_backward(void);
+void yellow_seg2_backward(void);
+void yellow_seg3_backward(void);
+void yellow_seg4_backward(void);
+
+void P2_on(void);
+void P2_off(void);
+
+#define YLED01 LATAbits.LATA0
+#define YLED02 LATAbits.LATA1
+#define YLED03 LATAbits.LATA2
+#define YLED04 LATAbits.LATA3
+#define YLED05 LATAbits.LATA4
+#define YLED06 LATAbits.LATA5
+#define YLED07 LATAbits.LATA7
+#define YLED08 LATAbits.LATA6
+#define YLED09 LATCbits.LATC0
+#define YLED10 LATCbits.LATC1
+#define LED_en LATCbits.LATC2
+#define WLED01 LATCbits.LATC3
+
+const uint8_t ON = 1;
+const uint8_t OFF = 0;
+const uint16_t PWM1_DutyCycle_100PERCENT = 799 + 1;
+const uint16_t PWM1_DutyCycle_60PERCENT = 479 + 1;
+const uint16_t PWM1_DutyCycle_6PERCENT = 47 + 1;
+const uint8_t TURN_DELAY_MS = 57;
+const uint8_t WELCOME_DELAY_MS = 100;
+const uint8_t TEST_DELAY_MS = 250;
+const uint16_t TURN_LIGHT_ON_600MS = 400;
+const uint16_t WELCOME_LIGHT_TOTAL_MS = 15000;
+const uint16_t TURN_LIGHT_OFF_600MS = 1000;
+const uint8_t II_CNT = 7;
+const uint8_t PWM_MAX = 10;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LED_H */
---
+++ TY1500_U3_V05.X/main.c
@@ -0,0 +1,103 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules
+  selected in the GUI. Generation Information : Product Revision  :  PIC10 /
+  PIC12 / PIC16 / PIC18 MCUs - 1.81.8 Device            :  PIC16F1936 Driver
+  Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software
+   and any derivatives exclusively with Microchip products. It is your
+   responsibility to comply with third party license terms applicable to your
+   use of third party software (including open source software) that may
+   accompany Microchip software.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
+    FOR A PARTICULAR PURPOSE.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
+    SOFTWARE.
+*/
+
+// 20260109 V01 CS:
+// IC1(IBV363) 第一次啟動時enable腳位要送100%的PWM值，之後再依需求調整PWM值
+// PWM 625HZ
+// 1.方向燈OK
+// 2.位置燈OK
+// 3.日行燈OK
+// 4.P2 OK
+// 5.白黃切換OK (行車&晝行燈白光或黃光)
+// 6.TEST POINT OK(短路方向燈速度變慢)
+
+//20260127 V02 CS:BE1D
+//1.有POS信號P2就亮.
+//2.方向燈流光 有方向燈信號就作動.(沒有方向燈信號時，立刻關閉方向燈)
+//3.迎賓燈 方向燈信號超過600ms,再等待600ms無方向燈信號作動.(有方向燈信號時，立刻停止迎賓燈)
+//4.DRL/POS 無迎賓燈旗標，沒有方向燈信號超過600ms才作動.
+//5.IC1(IBV363) 維持在PWM 100%(PWM 0%時，無法啟動)
+//6.DRL/POS 啟動後就不要再跑迎賓燈了.
+//7.DRL/POS 關閉 重置迎賓燈功能)
+//8.迎賓燈只能跑一次.
+
+//20260130 V03 CS:E3B3
+//修正變數 函數名稱
+
+//20260304 V04 CS:72DE
+//迎賓燈改用unlcok做觸發.
+
+//20260305 V05 CS:D5F8
+//修正迎賓燈一直送會重覆啟動迎賓燈
+
+
+#include "LED.h"
+#include <pic.h>
+#include <stdint.h>
+
+/*
+                         Main application
+ */
+int main(void) {
+  // initialize the device
+  SYSTEM_Initialize();
+  TMR0_SetInterruptHandler(tmr0_10ms_even);
+
+  all_led_off();
+  clear_no_call_warning_message();
+  LED_en = 1; // enable LED driver
+  
+  // Enable the Global Interrupts
+  INTERRUPT_GlobalInterruptEnable();
+  // Enable the Peripheral Interrupts
+  INTERRUPT_PeripheralInterruptEnable();
+
+  while (1) {
+    // Add your application code
+    LED_main();
+  }
+  return 0;
+}
+/**
+ End of File
+*/
